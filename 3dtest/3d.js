b28dc982eb01fa594c9b10de0ba2e97d7976a3b5
clam~ <pie314159265@users.noreply.github.com>
2017-05-13 10:14:10 -0400

    Update 3d.js

--- 3dtest/3d.js
+++ 3dtest/3d.js
@@ -1,4 +1,4 @@
-function sphericalTo3d(coords){
+function to3d(coords){
 	r=coords.r;
 	t1=coords.t1;
 	t2=coords.t2;
@@ -8,4 +8,13 @@ function sphericalTo3d(coords){
 	z=r*Math.sin(t2);
 	return {x:x,y:y,z:z};
 }
-function 3dToSpherical(coords){}
+function toSpherical(coords){
+	x=coords.x;
+	y=coords.y;
+	z=coords.z;
+	
+	t1=Math.atan(y/x);
+	t2=Math.atan(z/(y/Math.sin(t1)));
+	r=z/Math.sin(t2);
+	return {r:r,t1:t1,t2:t2};
+}
