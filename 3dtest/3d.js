1c2cd40e9afbdd7da115273e3c799bf350b5a3e2
clam~ <pie314159265@users.noreply.github.com>
2017-05-18 19:59:30 -0400

    Update 3d.js

--- 3dtest/3d.js
+++ 3dtest/3d.js
@@ -47,14 +47,25 @@ zoom = 1
 size = document.querySelector("#magic").width.baseVal.value;
 window.setInterval(function(){
     while(p.length){p[0].parentElement.removeChild(p[0]);p.shift();}
+    while(l.length){l[0].parentElement.removeChild(l[0]);l.shift();}
     if(tracking){}
     for(var i = 0;i<points.length;i++){
-        scalefactor=size/500*zoom*5/points[i].z;
+        scalefactor=size/500*zoom*50/points[i].z;
         a = document.createElementNS("http://www.w3.org/2000/svg","circle");
         a.setAttribute("cx",points[i].x*scalefactor+size/2);
         a.setAttribute("cy",points[i].y*scalefactor+size/2);
         a.setAttribute("r",5*scalefactor);
         document.querySelector("#magic").appendChild(a);
         p.push(a);
+    }
+    for(var i = 0;i<lines.length;i++){
+        scalefactor=size/500*zoom*50/points[i].z;
+        a = document.createElementNS("http://www.w3.org/2000/svg","line");
+        a.setAttribute("x1",points[lines[i][0]].x*scalefactor+size/2);
+        a.setAttribute("y1",points[lines[i][0]].y*scalefactor+size/2);
+        a.setAttribute("x2",points[lines[i][1]].x*scalefactor+size/2);
+        a.setAttribute("y2",points[lines[i][1]].y*scalefactor+size/2);
+        document.querySelector("#magic").appendChild(a);
+        l.push(a);
     }
 },200);
