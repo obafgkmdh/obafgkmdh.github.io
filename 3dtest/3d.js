de15640a66be4b29193d496d75abf580d6466adb
clam~ <pie314159265@users.noreply.github.com>
2017-05-19 17:30:44 -0400

    nobody likes radians but ok

--- 3dtest/3d.js
+++ 3dtest/3d.js
@@ -1,7 +1,7 @@
 function to3d(coords){
     r=coords.r;
-    t1=coords.t1;
-    t2=coords.t2;
+    t1=coords.t1*Math.PI/180;
+    t2=coords.t2*Math.PI/180;
     
     x=r*Math.cos(t2)*Math.cos(t1);
     y=r*Math.cos(t2)*Math.sin(t1);
@@ -13,9 +13,9 @@ function toSpherical(coords){
     y=coords.y;
     z=coords.z;
     
-    t1=Math.atan(y/x);
-    t2=Math.atan(z/(y/Math.sin(t1)));
-    r=z/Math.sin(t2);
+    t1=Math.atan(y/x)*180/Math.PI;
+    t2=Math.atan(z*180/Math.PI*Math.sin(t1*Math.PI/180)/y);
+    r=z/Math.sin(t2*Math.PI/180);
     return {r:r,t1:t1,t2:t2};
 }
 points = [{x:3,y:-4,z:12}, {x:-4,y:2,z:15}, {x:17,y:7,z:17}, {x:0,y:0,z:0}];
