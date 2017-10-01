81859fa78a7d0d6f14cc035e4b323c8c478c74fa
clamchowder <pie314159265@users.noreply.github.com>
2017-10-01 09:56:35 -0400

    Update index.js

--- codebreaker/index.js
+++ codebreaker/index.js
@@ -1,1 +1,10 @@
-
+function run(){
+    
+}
+var js_working=document.querySelector("#js-working");
+if(!windwo.Worker){
+    js_working.innerHTML="Your browser does not support Web Workers. This website will still work, just more slowly.";
+} else{
+    js_working.innerHTML="";
+    
+}
