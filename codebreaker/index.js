4bdf682dff6088956721f1657a3efbe62302efdf
clamchowder <pie314159265@users.noreply.github.com>
2017-10-01 10:50:26 -0400

    Update index.js

--- codebreaker/index.js
+++ codebreaker/index.js
@@ -1,21 +1,33 @@
-function run(){
-    document.getElementById("result").innerHTML=""; // clear
-    var type = document.getElementById("t").value;
-    /*var vars={
-        n:document.getElementById("n").value,
-        e:document.getElementById("e").value,
-        c:document.getElementById("c").value,
-        d:document.getElementById("d").value,
-        p:document.getElementById("p").value,
-        q:document.getElementById("q").value
-    };*/
+function run(webWorkersSupported){
+    var res = document.getElementById("result").innerHTML;
+    res.innerHTML="Result:<br>";
+    var type1 = document.getElementById("t").value;
+    var type2 = document.getElementById("t2").value;
+    if(type1=="e"){
+        if(webWorkersSupported){
+            //do stuff
+        }else{
+            //darn
+        }
+    }
+    elif(type1=="d"){
+        if(webWorkersSupported){
+            //do stuff
+        }else{
+            //darn
+        }
+    }
+    else{
+        res.innerHTML="really?!";
+    }
+    
 }
 setTimeout(function(){
     var js_working=document.querySelector("#js-working");
     if(!window.Worker){
         js_working.innerHTML="Your browser does not support Web Workers. This website will still work, just more slowly.";
-    } else{
+    }else{
         js_working.innerHTML="";
 
     }
-},50);
+},5);
