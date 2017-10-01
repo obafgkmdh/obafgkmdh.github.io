4800397d89255158c3b1adf8425b2f0735d5c38f
clamchowder <pie314159265@users.noreply.github.com>
2017-10-01 10:13:39 -0400

    Update index.js

--- codebreaker/index.js
+++ codebreaker/index.js
@@ -1,8 +1,16 @@
 function run(){
-    
+    var type = document.getElementById("t").value;
+    /*var vars={
+        n:document.getElementById("n").value,
+        e:document.getElementById("e").value,
+        c:document.getElementById("c").value,
+        d:document.getElementById("d").value,
+        p:document.getElementById("p").value,
+        q:document.getElementById("q").value
+    };*/
 }
 var js_working=document.querySelector("#js-working");
-if(!windwo.Worker){
+if(!window.Worker){
     js_working.innerHTML="Your browser does not support Web Workers. This website will still work, just more slowly.";
 } else{
     js_working.innerHTML="";
