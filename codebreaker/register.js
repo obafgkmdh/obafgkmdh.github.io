359e7c792e8e4e307048676274794706da1a3822
clamchowder <pie314159265@users.noreply.github.com>
2017-10-27 13:22:30 -0400

    Update register.js

--- codebreaker/register.js
+++ codebreaker/register.js
@@ -1,3 +1,10 @@
+function register(){
+    u=document.getElementById("user").value;
+    m=document.getElementById("mail").value;
+    p1=document.getElementById("pass").value;
+    p2=document.getElementById("cpass").value;
+    if(p1!=p2){console.log("passwords do not match")}
+};
 onload=function(){
     var js_working=document.querySelector("#js-working");
     js_working.innerHTML="";
