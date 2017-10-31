2b69c0c80cad8f0e18de46be734b2f54ecea2167
clamchowder <pie314159265@users.noreply.github.com>
2017-10-31 12:10:55 -0400

    Update caesar.js

--- codebreaker/caesar.js
+++ codebreaker/caesar.js
@@ -3,14 +3,15 @@ function write(m,e){
     postMessage(e==1?m.replace(/</g,"&lt;"):m);//plz no inject
 };
 function caesar(text, key){
+    key=bigInt(key);
     alphabetLower = "abcdefghijklmnopqrstuvwxyz";
     alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     caesared = "";
     for(var i = 0; i < text.length; i++){
         if(alphabetUpper.search(text[i])!==-1){
-            caesared+=alphabetUpper[(alphabetUpper.search(text[i])+key)%alphabetUpper.length];
+            caesared+=alphabetUpper[(bigInt(alphabetUpper.search(text[i])).plus(key)).mod(bigInt(alphabetUpper.length)).valueOf()];
         }else if(alphabetLower.search(text[i])!==-1){
-            caesared+=alphabetLower[(alphabetLower.search(text[i])+key)%alphabetLower.length];
+            caesared+=alphabetLower[(bigInt(alphabetLower.search(text[i])).plus(key)).mod(bigInt(alphabetLower.length)).valueOf()];
         }else{
             caesared+=text[i];
         }
@@ -23,8 +24,8 @@ onmessage = function(VARS){
         alphabetUpper = "abcdefghijklmnopqrstuvwxyz";
         alphabetLower = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         if(isNaN(parseInt(vars.k))){
+            write("Guessing key... ");
             for(var i = 0;i<26;i++){
-                write("Guessing key: ");
                 write("Decrypted message found: "+caesar(vars.c,i));
             }
         }else if(parseInt(vars.k)){
