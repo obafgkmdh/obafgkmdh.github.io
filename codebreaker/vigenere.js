7e6627168b10c542f43a9e30f64c6b0e333d1a6b
clamchowder <pie314159265@users.noreply.github.com>
2017-12-06 12:31:03 -0500

    Update vigenere.js

--- codebreaker/vigenere.js
+++ codebreaker/vigenere.js
@@ -32,15 +32,10 @@ onmessage = function(VARS){
         var vars = VARS.data;
         alphabetUpper = "abcdefghijklmnopqrstuvwxyz";
         alphabetLower = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        if(isNaN(parseInt(vars.k))){
-            write("Guessing key... <br>");
-            for(var i = 0;i<26;i++){
-                write("Decrypted message found: "+caesar(vars.c,i),1);
-                write("<br>");
-            }
-            write("END");
-        }else if(parseInt(vars.k)){
+        if(vars.k){
             write("Decrypted message found: "+caesar(vars.c,vars.e?parseInt(vars.k):-parseInt(vars.k)),1);
+            write("END");
+        }else{
             write("END");
         }
     }catch(err){
