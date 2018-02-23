7a1277541fe09011a636e07c68981e4cb5972611
clamchowder <pie314159265@users.noreply.github.com>
2018-02-23 13:00:00 -0500

    Update xor.js

--- codebreaker/xor.js
+++ codebreaker/xor.js
@@ -1,1 +1,20 @@
-
+importScripts('biginteger.js');
+function write(m,e){
+    postMessage(e==1?m.replace(/</g,"&lt;"):m);//plz no inject
+};
+function xor(text, key, e){
+    
+};
+onmessage = function(VARS){
+    try{
+        var vars = VARS.data;
+        if(vars.k){
+            write(!vars.e?"Decrypted message found: ":"Encrypted message: "+xor(vars.c,vars.k,vars.e),1);
+            write("END");
+        }else{
+            write("END");
+        }
+    }catch(err){
+        write("END");
+    }
+}
