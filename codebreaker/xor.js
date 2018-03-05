5e4a30f83a9aa24510d751388b5141d991a3ee90
clamchowder <pie314159265@users.noreply.github.com>
2018-03-05 11:33:45 -0500

    Update xor.js

--- codebreaker/xor.js
+++ codebreaker/xor.js
@@ -5,14 +5,22 @@ function write(m,e){
 function xor(text, key, e){
     t="";k="";a="";
     if(text.length>key.length){t=text;k=key.repeat(Math.ceil(text.length/key.length)).substr(0,text.length)}
-    for(var i = 0;i < t.length;i++){a+=String.fromCharCode(t[i].charCodeAt(0)^k[i].charCodeAt(0));}
+    if(e==="xia"){
+        for(var i = 0;i < t.length;i++){a+=String.fromCharCode(t[i].charCodeAt(0)^k[i].charCodeAt(0));}
+    }
+    if(e==="xih"){
+        for(var i = 0;i < t.length;i++){a+="0123456789abcdef"[parseInt(t[i],16)^parseInt(k[i],16)];}
+    }
+    if(e==="xib"){
+        for(var i = 0;i < t.length;i++){a+=""+((+t[i])^(+k[i]));}
+    }
     return a;
 };
 onmessage = function(VARS){
     try{
         var vars = VARS.data;
         if(vars.k){
-            write(!vars.e?"Decrypted message found: ":"Encrypted message: "+xor(vars.c,vars.k,vars.e),1);
+            write("Message: "+xor(vars.c,vars.k,vars.e),1);
             write("END");
         }else{
             write("END");
