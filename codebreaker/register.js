81bc3776e3c3f65314b0016fa583c9e43197940c
clamchowder <pie314159265@users.noreply.github.com>
2018-03-06 19:35:12 -0500

    oops

--- codebreaker/register.js
+++ codebreaker/register.js
@@ -6,7 +6,7 @@ function register(){
     if(p1!=p2){js_working.innerHTML="Passwords do not match!"}
     req = new XMLHttpRequest();
     req.open("POST","https://lamchcl.pythonanywhere.com/reg",true);
-    req.send("user="+urlencode(u)+"&pass="+urlencode(p1));
+    req.send("user="+encodeURI(u)+"&pass="+encodeURI(p1));
     req.onreadystatechange=function(){
         if(this.readyState==4&&this.status==200){
             if(this.responseText=="1"){
