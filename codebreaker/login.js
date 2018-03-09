48062089c3e9b32841c583104e21fc329ee67ec7
clamchowder <pie314159265@users.noreply.github.com>
2018-03-09 12:41:40 -0500

    Update login.js

--- codebreaker/login.js
+++ codebreaker/login.js
@@ -1,19 +1,36 @@
+function getCookie(cname) { // thanks w3schools
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 onload=function(){
     var js_working=document.querySelector("#js-working");
     js_working.innerHTML="";
+    if(getCookie("fawef")){
+       document.getElementById("form").innerHTML="You are already logged in!";
+    }
 }
 function login(){
     u = document.querySelector("#user").value;
     p = document.querySelector("#pass").value;
     req = new XMLHttpRequest();
     req.open("POST","https://lamchcl.pythonanywhere.com/log",true);
-    req.send("user="+encodeURI(u)+"&pass="+encodeURI(p));
+    req.send("user="+encodeURIComponent(u)+"&pass="+encodeURIComponent(p));
     req.onreadystatechange=function(){
         if(this.readyState==4&&this.status==200){
             js_working=document.querySelector("#js-working");
             if(this.responseText=="1"){
                 js_working.innerHTML="Logged in!" 
-                document.cookie=u;
+                document.cookie="fawef="+encodeURIComponent(u);
             }else{
                 js_working.innerHTML="Wrong username or password!"
             }
