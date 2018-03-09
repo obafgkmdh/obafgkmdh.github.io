fac8a2f03f95aacb0dc17fb053f3e79844de26dd
clamchowder <pie314159265@users.noreply.github.com>
2018-03-09 12:43:57 -0500

    Create logout.js

---
+++ codebreaker/logout.js
@@ -0,0 +1,5 @@
+onload=function(){
+    var js_working=document.querySelector("#js-working");
+    js_working.innerHTML="";
+    document.cookie = "fawef=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+}
