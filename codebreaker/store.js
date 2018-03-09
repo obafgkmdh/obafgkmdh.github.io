02cb7f14d97dae52cd7d61a161c673aa0e7ef66f
clamchowder <pie314159265@users.noreply.github.com>
2018-03-09 12:55:45 -0500

    Update store.js

--- codebreaker/store.js
+++ codebreaker/store.js
@@ -1,4 +1,23 @@
+function getCookie(cname) { // thanks w3schools
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 onload=function(){
     var js_working=document.querySelector("#js-working");
     js_working.innerHTML="";
-}
+    if(getCookie("fawef")){
+        document.querySelector("nav").removeChild(document.querySelector("nav").lastChild);
+        document.querySelector("nav").lastChild.innerHTML="LOGOUT";
+        document.querySelector("nav").lastChild.href="logout.html";
+    }
+};
