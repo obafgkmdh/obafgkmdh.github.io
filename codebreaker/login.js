db7e6b458e27cccd998c262abb472d231fb8a4c4
clamchowder <pie314159265@users.noreply.github.com>
2018-03-09 12:34:02 -0500

    Update login.js

--- codebreaker/login.js
+++ codebreaker/login.js
@@ -12,7 +12,8 @@ function login(){
         if(this.readyState==4&&this.status==200){
             js_working=document.querySelector("#js-working");
             if(this.responseText=="1"){
-                js_working.innerHTML="Logged in!"
+                js_working.innerHTML="Logged in!" 
+                document.cookie=u;
             }else{
                 js_working.innerHTML="Wrong username or password!"
             }
