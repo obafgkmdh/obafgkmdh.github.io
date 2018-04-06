928e335ebe6410fd7d1571e59746779e924096a9
clamchowder <pie314159265@users.noreply.github.com>
2018-04-06 13:25:30 -0400

    Create hash.js

---
+++ codebreaker/hash.js
@@ -0,0 +1,59 @@
+importScripts('biginteger.js');
+
+async function sha(message, h) {
+
+    // encode as UTF-8
+    const msgBuffer = new TextEncoder('utf-8').encode(message);
+
+    // hash the message
+    const hashBuffer = await crypto.subtle.digest(h, msgBuffer);
+    
+    // convert ArrayBuffer to Array
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+    // convert bytes to hex string
+    const hashHex = hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
+    return hashHex;
+}
+
+function write(m,e){
+    postMessage(e==1?m.replace(/</g,"&lt;"):m);//plz no inject
+};
+function hash(text, e, hash){
+    a="";
+    if(e==="e"){
+        switch(hash){
+            case "sha1":
+                (async function(){a=await sha(text, 'SHA-1');}());
+            break;
+            case "sha256":
+                (async function(){a=await sha(text, 'SHA-256');}());
+            break;
+            case "sha384":
+                (async function(){a=await sha(text, 'SHA-384');}());
+            break;
+            case "sha512":
+                (async function(){a=await sha(text, 'SHA-512');}());
+            break;
+            default:
+                a = "Hash type not supported."
+        }
+    }else{
+        a = "'Decryption' of hashes is not supported."
+    }
+    return a;
+};
+onmessage = function(VARS){
+    try{
+        var vars = VARS.data;
+        if(vars.k && vars.e){
+            write("Message: "+hash(vars.c,vars.e,vars.h),1);
+            write("END");
+        }else{
+            write("Please provide a key and an encryption/decryption mode!")
+            write("END");
+        }
+    }catch(err){
+        write("END");
+    }
+}
