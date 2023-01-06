9479ffc693c87b5962a9da54786f2f64a731741a
clamchowder <22942098+obafgkmdh@users.noreply.github.com>
2023-01-06 14:52:26 -0500

    clean slate

--- codebreaker/logout.js
+++
@@ -1,5 +0,0 @@
-onload=function(){
-    var js_working=document.querySelector("#js-working");
-    js_working.innerHTML="";
-    document.cookie = "fawef=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/codebreaker;";
-}
--- rsa/README.md
+++
@@ -1,1 +0,0 @@
-this is a tool that tries to do most of your rsa work for you
